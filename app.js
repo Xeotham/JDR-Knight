a9073275f5738514cc65e624f34fdd7e8524e03a
Maxence <maxenceouvrarddu16@outlook.fr>
2024-12-05 17:29:38 +0100

    interaction-front-back

--- app.js
+++ app.js
@@ -48,4 +48,65 @@ app.use('/api/pj', pjRoutes);
 app.use('/api/gears', gearRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+
 module.exports = app;
+
+//VARIABLES UTILES
+
+const API_URL = "http://localhost:3000/api";
+const TOKEN = localStorage.getItem("authToken");
+
+//FONCTION POUR INTERAGIR ENTRE LE FRONT ET LE BACK
+
+async function fetchGears() {
+  try{
+      const response = await fetch(`${API_URL}/gears`,{
+        headers: { Authorization: `Bearer ${TOKEN}`},
+      });
+      const gears = await response.json();
+      displayGears(gears);
+  }
+  catch(error)
+  {
+    console.error("Erreur lors de la recuperation des gears:", error);
+  }  
+}
+
+function displayGears(gears){
+  const gearlist = document.getElementById("gears-List");
+  gearsList.innerHTML = gears
+    .map(gear => `<div class="gear-item">${gear.gearName}</div>`)
+    .join("");
+}
+
+async function fetchPJs() {
+  try{
+      const response = await fetch(`${API_URL}/pj`,{
+        headers: { Authorization: `Bearer ${TOKEN}`},
+      });
+      const pjs = await response.json();
+      displayGears(pjs);
+  }
+  catch(error)
+  {
+    console.error("Erreur lors de la recuperation des PJs:", error);
+  }
+}
+
+function displayPJs(pjs){
+  const gearlist = document.getElementById("pjs-List");
+  PjList.innerHTML = pjs
+    .map(pj => `<div class="gear-item">${pj.Name}</div>`)
+    .join("");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (TOKEN)
+    {
+      fetchGears();
+      fetchPJs();
+    }
+    else{
+      alert("veuillez vous connecter !");
+    }
+});
